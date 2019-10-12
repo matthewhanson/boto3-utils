927a425fc865ad7763c4467b31e6f7e465a523a2
Matthew Hanson <matt.a.hanson@gmail.com>
2019-10-11 20:22:20 -0400

    comment out all of stepfunction tests, not sure how best to test

--- test/test_stepfunctions.py
+++ test/test_stepfunctions.py
@@ -3,17 +3,17 @@ import os
 import pytest
 
 # this must be imported before any boto3 module
-from moto import mock_sfn
+#from moto import mock_sfn
 
 from boto3utils import stepfunctions as sfn
 from shutil import rmtree
 
 
-@pytest.fixture(scope='function')
-def sfn_mock():
-    with mock_sfn():
-        client = boto3.client('stepfunctions')
-        yield client
+#@pytest.fixture(scope='function')
+#def sfn_mock():
+#    with mock_sfn():
+#        client = boto3.client('stepfunctions')
+#        yield client
 
 
 #def test_run_activity(sfn):
