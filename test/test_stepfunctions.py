f23df83526b56db71127b4def08d0821b0e71cb1
Matthew Hanson <matt.a.hanson@gmail.com>
2019-11-22 19:35:56 -0500

    disable stepfunction tests

--- test/test_stepfunctions.py
+++ test/test_stepfunctions.py
@@ -1,12 +1,12 @@
-import boto3
-import os
-import pytest
+#import boto3
+#import os
+#import pytest
 
 # this must be imported before any boto3 module
 #from moto import mock_sfn
 
-from boto3utils import stepfunctions as sfn
-from shutil import rmtree
+#from boto3utils import stepfunctions as sfn
+#from shutil import rmtree
 
 
 #@pytest.fixture(scope='function')
