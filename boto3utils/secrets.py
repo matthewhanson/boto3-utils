b86a880faa8c825f9ef10601ee52559446fe462e
Matthew Hanson <matt.a.hanson@gmail.com>
2020-02-28 23:09:49 -0500

    update get_secrets

--- boto3utils/secrets.py
+++ boto3utils/secrets.py
@@ -1,8 +1,11 @@
 import base64
 import boto3
 import json
+import logging
 
 from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
 
 
 def get_secret(secret_name):
@@ -21,28 +24,9 @@ def get_secret(secret_name):
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
-    except ClientError as e:
-        print(e)
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+    except ClientError as err:
+        logger.error(f"Error getting secret: {err}")
+        raise(err)
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
