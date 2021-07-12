3690880fbbb9c11d781feb475084da7e234ca7db
jkeifer <jkeifer@element84.com>
2021-07-12 08:50:40 -0700

    secrets: don't log error, leave to caller

--- boto3utils/secrets.py
+++ boto3utils/secrets.py
@@ -1,11 +1,6 @@
 import base64
 import boto3
 import json
-import logging
-
-from botocore.exceptions import ClientError
-
-logger = logging.getLogger(__name__)
 
 
 def get_secret(secret_name):
@@ -16,23 +11,19 @@ def get_secret(secret_name):
         service_name='secretsmanager'
     )
 
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # Will throw a botocore.exceptions.ClientError for any of
+    # the specific exceptions for the 'GetSecretValue' API.
     # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as err:
-        logger.error(f"Error getting secret: {err}")
-        raise(err)
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
+
+    # Decrypts secret using the associated KMS CMK.
+    # Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if 'SecretString' in get_secret_value_response:
+        secret = get_secret_value_response['SecretString']
     else:
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-            
-        return json.loads(secret)
+        secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+    return json.loads(secret)
