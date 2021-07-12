193cc6d4b867abe4ba78ee23071f29114d6ae3bb
jkeifer <jkeifer@element84.com>
2021-07-12 10:28:53 -0700

    secrets: add tests

---
+++ test/test_secrets.py
@@ -0,0 +1,41 @@
+import os
+import boto3
+import pytest
+import json
+import base64
+
+# this must be imported before any boto3 module
+from moto import mock_secretsmanager
+
+from boto3utils import secrets
+from botocore.exceptions import ClientError
+
+
+testpath = os.path.dirname(__file__)
+
+
+SECRET_NAME = 'secret'
+SECRET = {'mock_key': 'mock_val'}
+SECRET_STRING = json.dumps(SECRET)
+SECRET_BINARY = base64.b64encode(SECRET_STRING.encode())
+
+
+@mock_secretsmanager
+def test_get_secret_string():
+    boto3.session.Session().client('secretsmanager', region_name='us-west-2').create_secret(Name=SECRET_NAME, SecretString=SECRET_STRING)
+    secret = secrets.get_secret(SECRET_NAME)
+    assert(secret == SECRET)
+
+
+@mock_secretsmanager
+def test_get_secret_undef():
+    with pytest.raises(ClientError):
+        secret = secrets.get_secret(SECRET_NAME)
+
+
+@mock_secretsmanager
+def test_get_secret_binary():
+    boto3.session.Session().client('secretsmanager', region_name='us-west-2').create_secret(Name=SECRET_NAME, SecretBinary=SECRET_BINARY)
+    secret = secrets.get_secret(SECRET_NAME)
+    assert(secret == SECRET)
+    #client.create_secret(Name=SECRET_NAME, SecretBinary=SECRET_BINARY)
