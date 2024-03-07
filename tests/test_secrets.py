c7bdd060a3a1b0ef2d488e31a895f8dc5ecab604
Phil Varner <pvarner@element84.com>
2024-03-07 10:30:30 -0500

    add 3.11 and 3.12, switch lint/format stack, add pre-commit, loosen dev tools version constraints

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -5,8 +5,8 @@ import base64
 from boto3utils import secrets
 from botocore.exceptions import ClientError
 
-SECRET_NAME = 'secret'
-SECRET = {'mock_key': 'mock_val'}
+SECRET_NAME = "secret"
+SECRET = {"mock_key": "mock_val"}
 SECRET_STRING = json.dumps(SECRET)
 SECRET_BINARY = base64.b64encode(SECRET_STRING.encode())
 
@@ -31,7 +31,7 @@ def binary_secret(secretsmanager):
 
 def test_get_secret_string(secret):
     secret = secrets.get_secret(SECRET_NAME)
-    assert (secret == SECRET)
+    assert secret == SECRET
 
 
 def test_get_secret_undef(secretsmanager):
@@ -41,4 +41,4 @@ def test_get_secret_undef(secretsmanager):
 
 def test_get_secret_binary(binary_secret):
     secret = secrets.get_secret(SECRET_NAME)
-    assert (secret == SECRET)
+    assert secret == SECRET
